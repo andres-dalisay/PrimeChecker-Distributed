17e4ed31ee9618f720269085e52de54f713894f3
Antoinne Bryce Salvador <antoinne_salvador@dlsu.edu.ph>
2024-02-27 11:58:03 +0800

    initial client-master-slave setup with load balancing

--- PrimeChecker-Distributed/PrimeChecker-Server.cpp
+++ PrimeChecker-Distributed/PrimeChecker-Server.cpp
@@ -25,36 +25,18 @@ bool check_prime(const int& n) {
     return true;
 }
 
-void checkPrimeLoop(int start, int end) {
+void checkPrimeLoop(std::vector<int> checkArray, int start, int end) {
     for (int i = start; i <= end; i++) {
-        if (i < 2) continue;
-        if (check_prime(i)) {
+        if (checkArray[i] < 2) continue;
+        if (check_prime(checkArray[i])) {
             mtx.lock();
-            primes.push_back(i);
+            primes.push_back(checkArray[i]);
             mtx.unlock();
         }
     }
 }
 
-void handle_client(SOCKET client_socket) {
-    char buffer[1024] = { 0 };
-    recv(client_socket, buffer, sizeof(buffer), 0);
-
-    std::string strBuffer(buffer); 
-    std::stringstream ss(strBuffer);
-    std::string token;
-
-                                
-    getline(ss, token, ',');     // Get the first number before comma
-    int num1 = std::stoi(token); // Convert string to integer
-
-    
-    getline(ss, token);          // Get the second number after comma
-    int num2 = std::stoi(token); // Convert string to integer
-
-    std::cout << "Received task from client: " << num1 << num2 << std::endl;
-
-
+void handle_master(std::vector<int> master_task) {
     clock_t start, end;
     start = clock();
 
@@ -62,15 +44,17 @@ void handle_client(SOCKET client_socket) {
     std::vector<std::thread> threads;
     threads.reserve(THREAD_COUNT);
 
-    int split = num2 / THREAD_COUNT;
+    int split = master_task.size() / THREAD_COUNT;
 
     for (int i = 0; i < THREAD_COUNT; i++) {
-		int start = i * split;
-		int end = (i + 1) * split - 1;
+        int start = i * split;
+        int end = (i + 1) * split - 1;
+
         if (i == THREAD_COUNT - 1) {
-			end = num2;
+			end = master_task.size() - 1;
 		}
-		threads.emplace_back(checkPrimeLoop, start, end);
+
+		threads.emplace_back(checkPrimeLoop, master_task, start, end);
 	}
 
     for (auto& thread : threads) {
@@ -82,14 +66,6 @@ void handle_client(SOCKET client_socket) {
     double time_taken = double(end - start) / double(CLOCKS_PER_SEC);
     std::cout << "Time taken by program is : " << std::fixed << time_taken << std::setprecision(5);
     
-    int primesCount = primes.size();
-
-    // Process the task and get the result
-    char result[1024];
-    sprintf_s(result, "%d", primesCount);
-    send(client_socket, result, strlen(result), 0);
-
-    closesocket(client_socket);
 }
 
 int main() {
@@ -133,8 +109,53 @@ int main() {
             std::cerr << "Error accepting connection" << std::endl;
             continue;
         }
+        
+        //RECEIVE TASK
+        char buffer[1024] = { 0 };
+        recv(client_socket, buffer, sizeof(buffer), 0);
 
-        handle_client(client_socket);
+        std::string strBuffer(buffer);
+        std::stringstream ss(strBuffer);
+        std::string token;
+
+
+        getline(ss, token, ',');     // Get the first number before comma
+        int num1 = std::stoi(token); // Convert string to integer
+
+
+        getline(ss, token);          // Get the second number after comma
+        int num2 = std::stoi(token); // Convert string to integer
+
+        std::cout << "Received task from client: " << num1 << num2 << std::endl;
+
+        //SPLIT TASK
+        std::vector<int> master_task;
+        std::vector<int> slave_task;
+
+        // create a for loop to iterate through the range of numbers
+        for (int i = num1; i <= num2; i++) {
+            /*if (i % 2) {
+				master_task.push_back(i);
+            }
+            else {
+                slave_task.push_back(i);
+            }*/
+            master_task.push_back(i);
+		}
+
+        handle_master(master_task);
+        //handle_slave(slave_task);
+        
+
+        // Process the task and get the result
+        int primesCount = primes.size();
+        char result[1024];
+        sprintf_s(result, "%d", primesCount);
+        send(client_socket, result, strlen(result), 0);
+
+        closesocket(client_socket);
+
+        
     }
 
     closesocket(server_socket);
--- PrimeChecker-Slave/PrimeChecker-Slave.cpp
+++ PrimeChecker-Slave/PrimeChecker-Slave.cpp
@@ -1,20 +1,143 @@
-// PrimeChecker-Slave.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+// slave_server.cpp
 #include <iostream>
+#include <winsock2.h>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <thread>
+#include <mutex>
+#include <iomanip>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+#pragma comment(lib, "ws2_32.lib")
+
+
+#define THREAD_COUNT 16
+
+std::mutex mtx;
+std::vector<int> primes;
+
+bool check_prime(const int& n) {
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void checkPrimeLoop(int start, int end) {
+    for (int i = start; i <= end; i++) {
+        if (i < 2) continue;
+        if (check_prime(i)) {
+            mtx.lock();
+            primes.push_back(i);
+            mtx.unlock();
+        }
+    }
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void handle_client(SOCKET client_socket) {
+    char buffer[1024] = { 0 };
+    recv(client_socket, buffer, sizeof(buffer), 0);
+
+    std::string strBuffer(buffer);
+    std::stringstream ss(strBuffer);
+    std::string token;
+
+
+    getline(ss, token, ',');     // Get the first number before comma
+    int num1 = std::stoi(token); // Convert string to integer
+
+
+    getline(ss, token);          // Get the second number after comma
+    int num2 = std::stoi(token); // Convert string to integer
+
+    std::cout << "Received task from client: " << num1 << num2 << std::endl;
+
+
+    clock_t start, end;
+    start = clock();
+
+    // Create threads
+    std::vector<std::thread> threads;
+    threads.reserve(THREAD_COUNT);
+
+    int split = num2 / THREAD_COUNT;
+
+    for (int i = 0; i < THREAD_COUNT; i++) {
+        int start = i * split;
+        int end = (i + 1) * split - 1;
+        if (i == THREAD_COUNT - 1) {
+            end = num2;
+        }
+        threads.emplace_back(checkPrimeLoop, start, end);
+    }
+
+    for (auto& thread : threads) {
+        thread.join();
+    }
+
+    end = clock();
+
+    double time_taken = double(end - start) / double(CLOCKS_PER_SEC);
+    std::cout << "Time taken by program is : " << std::fixed << time_taken << std::setprecision(5);
+
+    int primesCount = primes.size();
+
+    // Process the task and get the result
+    char result[1024];
+    sprintf_s(result, "%d", primesCount);
+    send(client_socket, result, strlen(result), 0);
+
+    closesocket(client_socket);
+}
+
+int main() {
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        std::cerr << "Error initializing Winsock" << std::endl;
+        return -1;
+    }
+
+    SOCKET server_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (server_socket == INVALID_SOCKET) {
+        std::cerr << "Error creating socket" << std::endl;
+        WSACleanup();
+        return -1;
+    }
+
+    sockaddr_in server_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = INADDR_ANY;
+    server_address.sin_port = htons(5000);
+
+    if (bind(server_socket, reinterpret_cast<SOCKADDR*>(&server_address), sizeof(server_address)) == SOCKET_ERROR) {
+        std::cerr << "Error binding socket" << std::endl;
+        closesocket(server_socket);
+        WSACleanup();
+        return -1;
+    }
+
+    if (listen(server_socket, 5) == SOCKET_ERROR) {
+        std::cerr << "Error listening on socket" << std::endl;
+        closesocket(server_socket);
+        WSACleanup();
+        return -1;
+    }
+
+    std::cout << "Master server is running..." << std::endl;
+
+    while (true) {
+        SOCKET client_socket = accept(server_socket, NULL, NULL);
+        if (client_socket == INVALID_SOCKET) {
+            std::cerr << "Error accepting connection" << std::endl;
+            continue;
+        }
+
+        handle_client(client_socket);
+    }
+
+    closesocket(server_socket);
+    WSACleanup();
+    return 0;
+}
