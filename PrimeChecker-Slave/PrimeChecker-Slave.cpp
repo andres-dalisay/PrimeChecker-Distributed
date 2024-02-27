fefe947d108cf8c815a6fdbcb9fdeac5d733e85d
Antoinne Bryce Salvador <antoinne_salvador@dlsu.edu.ph>
2024-02-27 15:39:20 +0800

    implement slave process

--- PrimeChecker-Slave/PrimeChecker-Slave.cpp
+++ PrimeChecker-Slave/PrimeChecker-Slave.cpp
@@ -26,12 +26,12 @@ bool check_prime(const int& n) {
     return true;
 }
 
-void checkPrimeLoop(int start, int end) {
+void checkPrimeLoop(std::vector<int> checkArray, int start, int end) {
     for (int i = start; i <= end; i++) {
-        if (i < 2) continue;
-        if (check_prime(i)) {
+        if (checkArray[i] < 2) continue;
+        if (check_prime(checkArray[i])) {
             mtx.lock();
-            primes.push_back(i);
+            primes.push_back(checkArray[i]);
             mtx.unlock();
         }
     }
@@ -64,44 +64,28 @@ std::vector<int> deserializeVector(const std::vector<char>& bytes) {
     return vec;
 }
 
-void handle_client(SOCKET client_socket) {
-    char buffer[1024] = { 0 };
-    recv(client_socket, buffer, sizeof(buffer), 0);
-
-    std::string strBuffer(buffer);
-    std::stringstream ss(strBuffer);
-    std::string token;
-
-
-    getline(ss, token, ',');     // Get the first number before comma
-    int num1 = std::stoi(token); // Convert string to integer
-
-
-    getline(ss, token);          // Get the second number after comma
-    int num2 = std::stoi(token); // Convert string to integer
-
-    std::cout << "Received task from client: " << num1 << num2 << std::endl;
-
-
+void handle_slave(std::vector<int> slave_task) {
     clock_t start, end;
     start = clock();
 
     // Create threads
-    std::vector<std::thread> threads;
-    threads.reserve(THREAD_COUNT);
+    std::vector<std::thread> threads_slave;
+    threads_slave.reserve(THREAD_COUNT);
 
-    int split = num2 / THREAD_COUNT;
+    int split = slave_task.size() / THREAD_COUNT;
 
     for (int i = 0; i < THREAD_COUNT; i++) {
         int start = i * split;
         int end = (i + 1) * split - 1;
+
         if (i == THREAD_COUNT - 1) {
-            end = num2;
+            end = slave_task.size() - 1;
         }
-        threads.emplace_back(checkPrimeLoop, start, end);
+
+        threads_slave.emplace_back(checkPrimeLoop, slave_task, start, end);
     }
 
-    for (auto& thread : threads) {
+    for (auto& thread : threads_slave) {
         thread.join();
     }
 
@@ -110,14 +94,6 @@ void handle_client(SOCKET client_socket) {
     double time_taken = double(end - start) / double(CLOCKS_PER_SEC);
     std::cout << "Time taken by program is : " << std::fixed << time_taken << std::setprecision(5);
 
-    int primesCount = primes.size();
-
-    // Process the task and get the result
-    char result[1024];
-    sprintf_s(result, "%d", primesCount);
-    send(client_socket, result, strlen(result), 0);
-
-    closesocket(client_socket);
 }
 
 int main() {
@@ -172,6 +148,9 @@ int main() {
 				std::cout << num << " ";
 			}
             std::cout << std::endl;
+
+            handle_slave(task);
+
         }
 
         //handle_client(master_socket);
